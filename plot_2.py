4139836e44b347f8c920d2475e7fe6d4278a3775
Yi-Lin Sung <r06942076@ntu.edu.tw>
2020-02-23 01:06:49 +0800

    update plot_2.py

--- plot_2.py
+++ plot_2.py
@@ -3,6 +3,9 @@ import matplotlib
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+import seaborn as sns
 
 names = [
         # 'logs/tetris_single_ppo_skip4_grid_large',
@@ -11,8 +14,8 @@ names = [
         #  'logs/tetris_single_ppo_skip4_grid_large_mini32',]
         #  'logs/tetris_single_ppo_skip4_grid_large_ent0',]
         #  'logs/tetris_single_ppo_skip1_grid_large',]
-         'logs/tetris_single_ppo_skip8_grid_linear',
-         'logs/tetris_single_ppo_skip8_grid_large']
+         'logs/tetris_single_ppo_IOL_{c,h,d}_224_frames12_2mins',
+         'logs/tetris_single_ppo_IOL_{c,h,d}_224_frames8_2mins']
 
 rewards = []
 line_sents = []
@@ -51,36 +54,61 @@ for i, name in enumerate(names):
     rewards[i] = rewards[i][:j]
     line_sents[i] = line_sents[i][:j]
 
+## post-process data, set the iteration i ~ i + k to i
+freq = 25
+
+for i, name in enumerate(names):
+    for j, iteration in enumerate(iterations[i]):
+        if j % freq == 0:
+            pre = iteration
+        else:
+            iterations[i][j] = pre
 
 styles = [':', '-']
 
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w'] 
+colors_light = ['lightblue', 'lightgreen', 'mistyrose', 'lightcyan', 'violet', 
+    'lightyellow', 'lightgray', 'lightpink'] 
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'gray', 'pink'] 
 
-fig, ax1 = plt.subplots()
-
-ax1.set_xlabel('iterations')
-ax1.set_ylabel('rewards')
-
+# fig, ax1 = plt.subplots()
+sns.set()
 for i, name in enumerate(names):
-    ax1.plot(iterations[i], rewards[i], color=colors[i], label=name)
+    # df = pd.DataFrame(rewards[i])
+    # print(rewards[i])
+    x = np.asarray(iterations[i])
+    y = np.asarray(rewards[i])
+
+    ax = sns.lineplot(x=x, y=y, label=name)
+    # ax1.plot(iterations[i], df, colors_light[i])
+    # ax1.plot(iterations[i], df.rolling(50).mean(), colors[i], label=name)
 # ax1.tick_params(axis='y', labelcolor=color)
 
-plt.legend()
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+ax.set_xlabel('iterations')
+ax.set_ylabel('rewards')
+
+# plt.legend()
+# fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
 plt.savefig("rewards.png")
 
-fig, ax2 = plt.subplots()
+plt.clf()
+
+# fig, ax2 = plt.subplots()
 
 # ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
-ax2.set_ylabel('line_sents')  # we already handled the x-label with ax1
+
 for i, name in enumerate(names):
-    # print(line_sents[i])
-    ax2.plot(iterations[i], line_sents[i], color=colors[i], label=name)
+    x = np.asarray(iterations[i])
+    y = np.asarray(line_sents[i])
+
+    ax = sns.lineplot(x=x, y=y, label=name)
 
 plt.legend()
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+ax.set_xlabel('iterations')
+
+ax.set_ylabel('line_sents')  # we already handled the x-label with ax1
 
 plt.savefig("line.png")
